89610ea4b66f73030159111ca3bbbfd0f5ddb0ab
ALINAPIMP <alina.nepikta@gmail.com>
2024-01-29 11:42:09 +0200

    patikrinau EUROVAISTINE status_code

--- Baigiamasis.py
+++ Baigiamasis.py
@@ -2,3 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+alldata=[]
+for i in range(1,3):
+    url = f"https://www.eurovaistine.lt/paieska/rezultatai?q=paracetamolis&page={i}"
+response=requests.get(url)
+print(response.status_code)
+#     soup=BeautifulSoup(response.content,'html.parser')
