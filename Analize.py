46c0da5831d8e3fff45871bf6a443b424b68c10c
ALINAPIMP <alina.nepikta@gmail.com>
2024-01-30 14:21:03 +0200

    Eurovaistine  atnaujinta

---
+++ Analize.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+#
+# file_path = "C:/Users/GS/PycharmProjects/pythonKursas/gintarine2.csv"
+#
+# gintarine_df = pd.read_csv(file_path)
+#
+# gintarine_df['product__title'].str.split(',', expand=True)
+# gintarine_df =
+# print()
+
+# file_path = "C:/Users/GS/PycharmProjects/pythonKursas/eurovaistine.csv"
+# eurovaistine_df = pd.read_csv(file_path)
+# eurovaistine_df['Price'] = eurovaistine_df['Price'].replace(-2, '[+,]', regex=True).astype(float)
+# print()
+# products_df['Price'] = products_df['Price'].replace( regex=True).astype(float)
+# products_df['Quantity'] = products_df['Quantity'].replace('[+,]', '', regex=True).astype(float)
