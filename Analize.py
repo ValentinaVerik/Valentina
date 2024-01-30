b4673e6242ac50800b75262d8762e91b8395d439
ALINAPIMP <alina.nepikta@gmail.com>
2024-01-30 14:22:08 +0200

    Eurovaistine  atnaujinta

--- Analize.py
+++ Analize.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-#
+
 # file_path = "C:/Users/GS/PycharmProjects/pythonKursas/gintarine2.csv"
 #
 # gintarine_df = pd.read_csv(file_path)
@@ -9,9 +9,9 @@ import matplotlib.pyplot as plt
 # gintarine_df =
 # print()
 
-# file_path = "C:/Users/GS/PycharmProjects/pythonKursas/eurovaistine.csv"
-# eurovaistine_df = pd.read_csv(file_path)
-# eurovaistine_df['Price'] = eurovaistine_df['Price'].replace(-2, '[+,]', regex=True).astype(float)
-# print()
-# products_df['Price'] = products_df['Price'].replace( regex=True).astype(float)
+file_path = "C:/Users/GS/PycharmProjects/pythonKursas/eurovaistine.csv"
+eurovaistine_df = pd.read_csv(file_path)
+eurovaistine_df['Price'] = eurovaistine_df['Price'].replace(-2, '[+,]', regex=True).astype(float)
+
+# products_df['Price'] = products_df['Price'].replace('[€,]', '', regex=True).astype(float)
 # products_df['Quantity'] = products_df['Quantity'].replace('[+,]', '', regex=True).astype(float)
