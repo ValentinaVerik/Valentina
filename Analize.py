655c55da20dfb270e2e456b2465c79b8b86b27b9
ALINAPIMP <alina.nepikta@gmail.com>
2024-01-31 16:49:30 +0200

    suvienuodinome stulpelio pavadinimą

--- Analize.py
+++ Analize.py
@@ -46,30 +46,30 @@ GAVOME PAGRINDINE LENTELE
 """
 GINTARINE
 """
-file_path = "C:/Users/Vytautas/PycharmProjects/pythonKursas/Valentina/gintarineVA.csv"
-gintarine_df = pd.read_csv(file_path)
-
-gintarine_df[['PAVADIN', 'gintar_gramai', 'gintar_tipas', 'gintar_kiekis']] = gintarine_df['product__title'].str.split(",", n=3, expand=True)
-# print(gintarine_df)
-
-"""
-GAVOME PAGRINDINE LENTELE
-"""
-
-df_gintarine=pd.DataFrame(gintarine_df)
-df_gintarine.to_csv('gintarineVA.csv', index=False)
-# print(df_gintarine)
+# file_path = "C:/Users/Vytautas/PycharmProjects/pythonKursas/Valentina/gintarineVA.csv"
+# gintarine_df = pd.read_csv(file_path)
+#
+# gintarine_df[['PAVADIN', 'gintar_gramai', 'gintar_tipas', 'gintar_kiekis']] = gintarine_df['product__title'].str.split(",", n=3, expand=True)
+# # print(gintarine_df)
+#
+# """
+# GAVOME PAGRINDINE LENTELE
+# """
+#
+# df_gintarine=pd.DataFrame(gintarine_df)
+# df_gintarine.to_csv('gintarineVA.csv', index=False)
+# # print(df_gintarine)
 
 """
 PRATRYNEM STULPELIUS
 """
 
-gintarine1_df = gintarine_df.drop(gintarine_df.columns[[0,1,3]], axis = 1)
-gintarine2_df = gintarine1_df.rename(columns={'product__price--regular':'gintarine_kaina'})
-# # print(gintarine1_df)
-df_gintarine2 = pd.DataFrame(gintarine2_df)
-df_gintarine2.to_csv('2gintarineVA.csv', index=False)
-print(df_gintarine2)
+# gintarine1_df = gintarine_df.drop(gintarine_df.columns[[0,1,3]], axis = 1)
+# gintarine2_df = gintarine1_df.rename(columns={'product__price--regular':'gintarine_kaina'})
+# # # print(gintarine1_df)
+# df_gintarine2 = pd.DataFrame(gintarine2_df)
+# df_gintarine2.to_csv('2gintarineVA.csv', index=False)
+# print(df_gintarine2)
 
 """
 GAVOME KAINOS VIDURKI PAGAL VAISTINE
@@ -84,26 +84,27 @@ GAVOME KAINOS VIDURKI PAGAL VAISTINE
 METU
 """
 
-# file_path = "C:/Users/Vytautas/PycharmProjects/pythonKursas/Valentina/metu_VA.csv"
-# metu_df = pd.read_csv(file_path)
-#
-# metu_df[['metu_pavadinimas', 'metu_gramai', 'metu_tipas', 'metu_kiekis']] = metu_df['1'].str.split(",", n=3, expand=True)
+file_path = "C:/Users/Vytautas/PycharmProjects/pythonKursas/Valentina/metu_VA.csv"
+metu_df = pd.read_csv(file_path)
+
+metu_df[['PAVADIN', 'metu_gramai', 'metu_tipas', 'metu_kiekis']] = metu_df['1'].str.split(",", n=3, expand=True)
 # print(metu_df)
 """
 GAVOME PAGRINDINĘ LENTELĘ
 """
-# df_metu=pd.DataFrame(metu_df)
-# df_metu.to_csv('metu_VA.csv', index=False)
+df_metu=pd.DataFrame(metu_df)
+df_metu.to_csv('metu_VA.csv', index=False)
 # print(df_metu)
 """
 PRATRYNĖM LENTELĘ
 """
-# metu1_df = metu_df.drop(metu_df.columns[[0,1]], axis = 1)
-# metu2_df = metu1_df.rename(columns={'2':'metu_kaina'})
-# # print(metu1_df)
-# df_metu2 = pd.DataFrame(metu2_df)
-# df_metu2.to_csv('2metuVA.csv', index=False)
+metu1_df = metu_df.drop(metu_df.columns[[0,1,3]], axis = 1)
+metu2_df = metu1_df.rename(columns={'2':'metu_kaina'})
+# print(metu1_df)
 
+df_metu2 = pd.DataFrame(metu2_df)
+df_metu2.to_csv('2metuVA.csv', index=False)
+print(df_metu2)
 """
 GAVOME KAINOS VIDURKI PAGAL VAISTINE
 """
