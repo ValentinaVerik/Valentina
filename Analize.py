eff7354a0b3b661726e33c1910d7526ee7fae346
ALINAPIMP <alina.nepikta@gmail.com>
2024-01-31 12:45:13 +0200

    iš puslapių ištraukti duomenys - Duomenis Sutvarkytos lentelės - amalizė

--- Analize.py
+++ Analize.py
@@ -1,22 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_path = "C:/Users/Vytautas/PycharmProjects/pythonKursas/Valentina/gintarine2.csv"
 
-gintarine_df = pd.read_csv(file_path)
-
-# gintarine_df[['pavadinimas', 'kita']] = gintarine_df['product__title'].str.split(n=1, expand=True)
-gintarine_df[['pavadinimas', 'gramai', 'tipas', 'kiekis']] = gintarine_df['product__title'].str.split(",", n=3, expand=True)
-# print(gintarine_df)
-
-df_gintarine=pd.DataFrame(gintarine_df)
-df_gintarine.to_csv('gintarine2.csv', index=False)
-print(df_gintarine)
-
-# #
-# file_path = "C:/Users/GS/PycharmProjects/pythonKursas/eurovaistine.csv"
+# file_path = "C:/Users/Vytautas/PycharmProjects/pythonKursas/Valentina/eurovaistineVA.csv"
 # eurovaistine_df = pd.read_csv(file_path)
-# eurovaistine_df['Price'] = eurovaistine_df['Price'].replace(n=-2, '[,]', regex=True).astype(float)
-# #
-# # products_df['Price'] = products_df['Price'].replace('[€,]', '', regex=True).astype(float)
-# products_df['Quantity'] = products_df['Quantity'].replace('[+,]', '', regex=True).astype(float)
+#
+# eurovaistine_df[['euro_pavadinimas', 'euro_gramai', 'euro_tipas', 'euro_kiekis']] = eurovaistine_df['pavadinimas'].str.split(",", n=3, expand=True)
+# # print(eurovaistine_df)
+# # eurovaistine_df = eurovaistine.df('kaina').replace
+#
+# df_eurovaistine = pd.DataFrame(eurovaistine_df)
+# df_eurovaistine.to_csv('eurovaistineVA.csv', index=False)
+# print(df_eurovaistine)
+
+
+# file_path = "C:/Users/Vytautas/PycharmProjects/pythonKursas/Valentina/gintarineVA.csv"
+# gintarine_df = pd.read_csv(file_path)
+#
+# gintarine_df[['gintar_pavadinimas', 'gintar_gramai', 'gintar_tipas', 'gintar_kiekis']] = gintarine_df['product__title'].str.split(",", n=3, expand=True)
+# # print(gintarine_df)
+#
+# df_gintarine=pd.DataFrame(gintarine_df)
+# df_gintarine.to_csv('gintarineVA.csv', index=False)
+# print(df_gintarine)
+
+file_path = "C:/Users/Vytautas/PycharmProjects/pythonKursas/Valentina/metu_VA.csv"
+metu_df = pd.read_csv(file_path)
+
+metu_df[['metu_pavadinimas', 'metu_gramai', 'metu_tipas', 'metu_kiekis']] = metu_df['1'].str.split(",", n=3, expand=True)
+# print(metu_df)
+
+df_metu=pd.DataFrame(metu_df)
+df_metu.to_csv('metu_VA.csv', index=False)
+print(df_metu)
--- Duomenys.py
+++ Duomenys.py
@@ -2,34 +2,34 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import json
-
-eurovaistine_data = []
-for i in range(1, 3):
-    target = f"https://www.eurovaistine.lt/paieska/rezultatai?q=paracetamolis&page={i}"
-    response = requests.get(target)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    script_tags = soup.find_all('script', type='application/json')
-
-    for script in script_tags:
-        data_component_name = script.get('data-component-name')
-        if data_component_name and 'ProductsList' in data_component_name:
-            products_json = json.loads(script.string)
-            for product in products_json['products']:
-                for variant in product['variants']:
-                    item = {
-                        'pavadinimas': variant['name'],
-                        'gamintojas': variant['brand'],
-                        'kaina': variant['price']['price'] #.replace(-2, '.').astype(float)
-                    }
-                    eurovaistine_data.append(item)
-
-json_data = json.dumps(eurovaistine_data, indent=4, ensure_ascii=False)
-# print(json_data)
-
-
-df_eurovaistine = pd.DataFrame(eurovaistine_data)
-df_eurovaistine.to_csv('eurovaistine.csv', index=False)
-print(df_eurovaistine)
+#
+# eurovaistine_data = []
+# for i in range(1, 3):
+#     target = f"https://www.eurovaistine.lt/paieska/rezultatai?q=paracetamolis&page={i}"
+#     response = requests.get(target)
+#     soup = BeautifulSoup(response.content, 'html.parser')
+#     script_tags = soup.find_all('script', type='application/json')
+#
+#     for script in script_tags:
+#         data_component_name = script.get('data-component-name')
+#         if data_component_name and 'ProductsList' in data_component_name:
+#             products_json = json.loads(script.string)
+#             for product in products_json['products']:
+#                 for variant in product['variants']:
+#                     item = {
+#                         'pavadinimas': variant['name'],
+#                         'gamintojas': variant['brand'],
+#                         'kaina': variant['price']['price'] #.replace(-2, '.').astype(float)
+#                     }
+#                     eurovaistine_data.append(item)
+#
+# json_data = json.dumps(eurovaistine_data, indent=4, ensure_ascii=False)
+# # print(json_data)
+#
+#
+# df_eurovaistine = pd.DataFrame(eurovaistine_data)
+# df_eurovaistine.to_csv('eurovaistine.csv', index=False)
+# print(df_eurovaistine)
 
 
 
@@ -47,7 +47,7 @@ print(df_eurovaistine)
 #         title = product.find('div', class_='product__title').text.strip()
 #         price = product.find('span', class_='product__price--regular')
 #         if price:
-#             price_text = price.text.strip().replace('€', '')
+#             price_text = price.text.strip().replace('€', '').replace(',', '.')
 #         else:
 #             price_text = " N/A "
 #         gintarine_data.append({
